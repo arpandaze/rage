5ee80f8be296b3b10ac1561d3f27cb2c1f97c618
Arpan Koirala <dazehere@yandex.com>
2022-02-13 14:47:55 +0545

    Multiple changes * appendonly.aof added to .gitignore * Email field on user table changed to UNIQUE * rustfmt.toml blank line removed

--- migrations/20211221103844_user_table.sql
+++ migrations/20211221103844_user_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   first_name VARCHAR(15) NOT NULL,
   middle_name VARCHAR(15),
   last_name VARCHAR(15) NOT NULL,
-  email VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
   phone VARCHAR(10),
   two_fa_secret VARCHAR,
   hashed_password VARCHAR,
@@ -11,6 +11,6 @@ CREATE TABLE users (
   is_active BOOLEAN DEFAULT(True),
   user_type SMALLINT NOT NULL DEFAULT(0),
   access_zones SMALLINT[],
-  created_on TIMESTAMP without time zone DEFAULT NOW()
+  created_on TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 );
 
